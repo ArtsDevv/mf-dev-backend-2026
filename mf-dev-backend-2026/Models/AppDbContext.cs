80c5afadfa4f341c18dbab7b39b03edc21ad8ced
Arthur Lobo <arthur.e.a.lobo@gmail.com>
2026-04-29 20:52:12 -0300

    add dbset

--- mf-dev-backend-2026/Models/AppDbContext.cs
+++ mf-dev-backend-2026/Models/AppDbContext.cs
@@ -6,5 +6,7 @@ namespace mf_dev_backend_2026.Models
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Veículo> Veiculos { get; set; }
     }
 }
