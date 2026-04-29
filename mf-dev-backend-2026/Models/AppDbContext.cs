299271aabaa5acd57e9ffb06729d5ae48b33e858
Arthur Lobo <arthur.e.a.lobo@gmail.com>
2026-04-29 20:49:54 -0300

    Att

--- mf-dev-backend-2026/Models/AppDbContext.cs
+++ mf-dev-backend-2026/Models/AppDbContext.cs
@@ -1,6 +1,10 @@
-﻿namespace mf_dev_backend_2026.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mf_dev_backend_2026.Models
+
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     }
 }
